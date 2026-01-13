cb95626953418b82498b361d7628d29f9101418b
keekz1 <148965858+keekz1@users.noreply.github.com>
2026-01-13 16:29:25 +0000

    Initial Django backend deployment

--- studypilot/settings.py
+++ studypilot/settings.py
@@ -157,6 +157,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files
 MEDIA_URL = '/media/'
